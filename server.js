b0b7942d142f3850f70441cdc8eeb53493da7445
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-10 22:38:34 -0400

    m

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const path = require('path');
+const cool = require('cool-ascii-faces');
+const PORT = process.env.PORT || 3000
+
+var ms = require('./mathService');
+
+app.set('view engine', 'ejs');
+app.set("port", (process.env.PORT || 3000));
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", getData);
+
+app.post("/", postData);
+
+app.get("/cool", (req, res) => res.send(cool()));
+
+app.listen(app.get("port"), function() {
+  console.log("Now listening on port: ", app.get("port"));
+});
+
+function getData(req, res) {
+  console.log("Getting data");
+
+  res.render('result', { var1: '', sign: '', var2: '', result: '' });
+}
+
+function postData(req, res) {
+  console.log("Posting data");
+  console.log(req.body.var1);
+  console.log(req.body.sign);
+  console.log(req.body.var2);
+  
+  var result = ms.computeOperation(req.body.sign, req.body.var1, req.body.var2);
+  console.log(result);
+  res.render('result', { var1: req.body.var1, sign: req.body.sign, var2: req.body.var2, result: result });
+}
